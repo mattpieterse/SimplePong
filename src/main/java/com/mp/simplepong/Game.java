c5491c2394d04ec5b558bebaf136fee869386ef8
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2023-12-19 23:58:51 +0200

    Update collision detection

--- src/main/java/com/mp/simplepong/Game.java
+++ src/main/java/com/mp/simplepong/Game.java
@@ -3,6 +3,7 @@ package com.mp.simplepong;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -91,9 +92,26 @@ public class Game extends JPanel
         
         if (isAlive) {
             
+            // update dynamic collision boxes
+            Rectangle ballCollider = new Rectangle(/* TODO */);
+            Rectangle playColliderA = new Rectangle(/* TODO */);
+            Rectangle playColliderB = new Rectangle(/* TODO */);
+            
+            // change ball direction upon collision with any paddle
+            if (collisionExists(ballCollider, playColliderA, playColliderB)) {
+                dirXB = -dirXB;
+            }
+            
         }
         
         repaint();
+    }
+    
+    private boolean collisionExists(Rectangle pong, Rectangle playerA, Rectangle playerB) {
+        if (pong.intersects(playerA) || pong.intersects(playerB)) {
+            return true;
+        }
+        return false;
     }
     
 }
