2dcd98493949af287af6ca2d0fc42944443f1927
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2023-12-19 23:34:51 +0200

    Call game screen from main class

--- src/main/java/com/mp/simplepong/App.java
+++ src/main/java/com/mp/simplepong/App.java
@@ -7,6 +7,7 @@ public class App {
 
     public static void main(String[] args) {
         
+        Game game = new Game();
         JFrame scene = new JFrame();
         
         scene.setSize(800, 600);
@@ -14,6 +15,7 @@ public class App {
         scene.setTitle("SimplePong (0.0.1)");
         scene.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         scene.setResizable(false);
+        scene.add(game);
         scene.setVisible(true);
     }
 }
