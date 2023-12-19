02869495bd01946a19c091dcdc9c3664854fc326
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2023-12-19 22:14:40 +0200

    Create method to handle gameplay

---
+++ src/main/java/com/mp/simplepong/Game.java
@@ -0,0 +1,31 @@
+
+package com.mp.simplepong;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
+
+public class Game extends JPanel 
+  implements KeyListener, ActionListener {
+    
+    // <editor-fold defaultstate="collapsed" desc="Unused Overrides">
+    @Override
+    public void keyTyped(KeyEvent e) {}
+    
+    @Override
+    public void keyReleased(KeyEvent e) {}
+    // </editor-fold>
+    
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO
+    }
+    
+}
