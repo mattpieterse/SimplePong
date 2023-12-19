35e6be8b4d278b0c65f93613b86fac0f1de7f8f5
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2023-12-19 22:07:40 +0200

    Add frame to contain game

--- src/main/java/com/mp/simplepong/App.java
+++ src/main/java/com/mp/simplepong/App.java
@@ -1,9 +1,19 @@
 
 package com.mp.simplepong;
 
+import javax.swing.JFrame;
+
 public class App {
 
     public static void main(String[] args) {
         
+        JFrame scene = new JFrame();
+        
+        scene.setSize(700, 600);
+        scene.setLocationRelativeTo(null);
+        scene.setTitle("SkeetPong (0.0.1)");
+        scene.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        scene.setResizable(false);
+        scene.setVisible(true);
     }
 }
