a40da1ca1cf39df6931784e5f9544dadc3166fd0
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2023-12-20 12:05:33 +0200

    Add final variable for screen size

--- src/main/java/com/mp/simplepong/App.java
+++ src/main/java/com/mp/simplepong/App.java
@@ -4,13 +4,16 @@ package com.mp.simplepong;
 import javax.swing.JFrame;
 
 public class App {
+    
+    public static final int SIZE_W = 800;
+    public static final int SIZE_H = 600;
 
     public static void main(String[] args) {
         
         Game game = new Game();
         JFrame scene = new JFrame();
         
-        scene.setSize(800, 600);
+        scene.setSize(SIZE_W, SIZE_H);
         scene.setLocationRelativeTo(null);
         scene.setTitle("SimplePong (0.0.1)");
         scene.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
